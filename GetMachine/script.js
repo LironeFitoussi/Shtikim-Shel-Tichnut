8c582b90794444440a0de4c2daa47dcc363d5aea
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-05-20 20:17:36 +0300

    minor fix

--- GetMachine/script.js
+++ GetMachine/script.js
@@ -2,14 +2,14 @@ function getOS() {
     const platform = navigator.userAgent;
 
     switch (true) {
+        case /iPhone/.test(platform):
+            return 'iOS';
         case /Mac/.test(platform):
             return 'Mac';
         case /Win/.test(platform):
             return 'Windows';
         case /Linux/.test(platform):
             return 'Linux';
-        case /iPhone|iPad|iPod/.test(platform):
-            return 'iOS';
         case /Android/.test(platform):
             return 'Android';
         default:
