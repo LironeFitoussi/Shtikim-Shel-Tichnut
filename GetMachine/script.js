0132b0ee2da055ae79febc799675c01fad7c356b
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-05-20 20:44:17 +0300

    final state

--- GetMachine/script.js
+++ GetMachine/script.js
@@ -20,7 +20,52 @@ function getOS() {
     return 'Unknown';
 }
 
-document.getElementById('detectBtn').addEventListener('click', function() {
-    const os = getOS();
-    document.getElementById('osDisplay').textContent = `Your operating system is: ${os}`;
-});
+// Predefined resources based on the detected OS
+const osResources = {
+  'Windows': {
+    text: 'Download for Windows',
+    color: '#0078d7',
+    link: 'https://en.wikipedia.org/wiki/Microsoft_Windows',
+    icon: 'windows.png'
+  },
+  'Mac': {
+    text: 'Download for macOS',
+    color: '#888888',
+    link: 'https://en.wikipedia.org/wiki/MacOS',
+    icon: 'mac.png'
+  },
+  'iOS': {
+    text: 'Download for iOS',
+    color: '#5555ff',
+    link: 'https://en.wikipedia.org/wiki/IOS',
+    icon: 'ios.png'
+  },
+  'Android': {
+    text: 'Download for Android',
+    color: '#3ddc84',
+    link: 'https://en.wikipedia.org/wiki/Android_(operating_system)',
+    icon: 'android.png'
+  },
+  'Linux': {
+    text: 'Download for Linux',
+    color: '#f0ad4e',
+    link: 'https://en.wikipedia.org/wiki/Linux',
+    icon: 'linux.png'
+  },
+  'Unknown': {
+    text: 'Download',
+    color: '#888',
+    link: 'https://example.com/unknown-download',
+    icon: 'unknown.png'
+  }
+};
+
+// Detect the user's OS
+const userOS = getOS();
+
+// Update the UI based on the detected OS
+document.getElementById('icon').innerHTML = `<img class="img-icon" src="icons/${osResources[userOS].icon}" alt="${userOS} icon">`;
+document.getElementById('text').innerText = osResources[userOS].text;
+document.getElementById('download').innerText = 'Download';
+document.getElementById('download').href = osResources[userOS].link;
+document.querySelector('.download-box').style.backgroundColor = osResources[userOS].color;
