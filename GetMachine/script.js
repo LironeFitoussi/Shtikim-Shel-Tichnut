c06dcd43b6de1599a9cfea866994d43efe428104
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2024-06-04 10:57:56 +0300

    Upadare Files For Github

--- GetMachine/script.js
+++ GetMachine/script.js
@@ -1,71 +1,68 @@
-function getOS() {
-    const userAgent = navigator.userAgent;
-
-    if (/Windows/.test(userAgent)) {
-        return 'Windows';
+async function getOS() {
+  try {
+    const { platform, brands} = await navigator.userAgentData;
+    if (platform === "Windows") return "Windows";
+    if (platform === "Macintosh") return "Mac";
+    for (const brand of brands) {
+      if (brand.brand === "Android") return "Android";
+      if (brand.brand === "iPhone" || brand.brand === "iPad") return "iOS";
     }
-    if (/Macintosh|MacIntel|MacPPC|Mac68K/.test(userAgent)) {
-        return 'Mac';
-    }
-    if (/iPhone|iPad|iPod/.test(userAgent)) {
-        return 'iOS';
-    }
-    if (/Android/.test(userAgent)) {
-        return 'Android';
-    }
-    if (/Linux/.test(userAgent)) {
-        return 'Linux';
-    }
-
-    return 'Unknown';
+    return "Linux"; // Assuming Linux for other unknown platforms
+  } catch (err) {
+    console.error("Error getting user agent data:", err);
+    return "Unknown";
+  }
 }
 
-// Predefined resources based on the detected OS
 const osResources = {
-  'Windows': {
-    text: 'Download for Windows',
-    color: '#0078d7',
-    link: 'https://en.wikipedia.org/wiki/Microsoft_Windows',
-    icon: 'windows.png'
+  Windows: {
+    text: "Download for Windows",
+    color: "#0078d7",
+    link: "https://en.wikipedia.org/wiki/Microsoft_Windows",
+    icon: "windows.png",
   },
-  'Mac': {
-    text: 'Download for macOS',
-    color: '#888888',
-    link: 'https://en.wikipedia.org/wiki/MacOS',
-    icon: 'mac.png'
+  Mac: {
+    text: "Download for macOS",
+    color: "#888888",
+    link: "https://en.wikipedia.org/wiki/MacOS",
+    icon: "mac.png",
   },
-  'iOS': {
-    text: 'Download for iOS',
-    color: '#5555ff',
-    link: 'https://en.wikipedia.org/wiki/IOS',
-    icon: 'ios.png'
+  iOS: {
+    text: "Download for iOS",
+    color: "#5555ff",
+    link: "https://en.wikipedia.org/wiki/IOS",
+    icon: "ios.png",
   },
-  'Android': {
-    text: 'Download for Android',
-    color: '#3ddc84',
-    link: 'https://en.wikipedia.org/wiki/Android_(operating_system)',
-    icon: 'android.png'
+  Android: {
+    text: "Download for Android",
+    color: "#3ddc84",
+    link: "https://en.wikipedia.org/wiki/Android_(operating_system)",
+    icon: "android.png",
   },
-  'Linux': {
-    text: 'Download for Linux',
-    color: '#f0ad4e',
-    link: 'https://en.wikipedia.org/wiki/Linux',
-    icon: 'linux.png'
+  Linux: {
+    text: "Download for Linux",
+    color: "#f0ad4e",
+    link: "https://en.wikipedia.org/wiki/Linux",
+    icon: "linux.png",
   },
-  'Unknown': {
-    text: 'Download',
-    color: '#888',
-    link: 'https://example.com/unknown-download',
-    icon: 'unknown.png'
-  }
+  Unknown: {
+    text: "Download",
+    color: "#888",
+    link: "https://example.com/unknown-download",
+    icon: "unknown.png",
+  },
 };
 
-// Detect the user's OS
-const userOS = getOS();
+async function updateUI() {
+  const userOS = await getOS();
+  document.getElementById(
+    "icon"
+  ).innerHTML = `<img class="img-icon" src="icons/${osResources[userOS].icon}" alt="${userOS} icon">`;
+  document.getElementById("text").innerText = osResources[userOS].text;
+  document.getElementById("download").innerText = "Download";
+  document.getElementById("download").href = osResources[userOS].link;
+  document.querySelector(".download-box").style.backgroundColor =
+    osResources[userOS].color;
+}
 
-// Update the UI based on the detected OS
-document.getElementById('icon').innerHTML = `<img class="img-icon" src="icons/${osResources[userOS].icon}" alt="${userOS} icon">`;
-document.getElementById('text').innerText = osResources[userOS].text;
-document.getElementById('download').innerText = 'Download';
-document.getElementById('download').href = osResources[userOS].link;
-document.querySelector('.download-box').style.backgroundColor = osResources[userOS].color;
+updateUI();
